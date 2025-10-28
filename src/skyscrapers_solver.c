538d9cde555e7fc4e4a1b48341a20dc00bf6f1ea
JulesR0land <julesf.roland@gmail.com>
2025-10-28 22:57:48 +0100

    Fix of a check win problem that would let rows and columns have several of the same number

--- src/skyscrapers_solver.c
+++ src/skyscrapers_solver.c
@@ -522,12 +522,49 @@ static bool clue_checker(board_info_t *b, uint32_t clue_index) {
 }
 
 
+
+static bool check_row_legality(board_info_t *b, uint32_t row) {
+    uint32_t *checker = calloc(b->ss, sizeof(uint32_t));
+
+    for (uint32_t col = 0; col < b->ss; col++) {
+        if (checker[b->board[row][col].answer - 1] > 0) {
+            free(checker);
+            return false;
+        }
+        checker[b->board[row][col].answer - 1]++;
+    }
+
+    free(checker);
+    return true;
+}
+
+static bool check_col_legality(board_info_t *b, uint32_t col) {
+    uint32_t *checker = calloc(b->ss, sizeof(uint32_t));
+
+    for (uint32_t row = 0; row < b->ss; row++) {
+        if (checker[b->board[row][col].answer - 1] > 0) {
+            free(checker);
+            return false;
+        }
+        checker[b->board[row][col].answer - 1]++;
+    }
+
+    free(checker);
+    return true;
+}
+
+
 static bool check_win(board_info_t *b) {
-    for (uint32_t row = 0; row < b->ss; row++)
-        for (uint32_t col = 0; col < b->ss; col++)
+    for (uint32_t row = 0; row < b->ss; row++) {
+        if (!check_row_legality(b, row) || !check_col_legality(b, row)) {
+            return false;
+        }
+        for (uint32_t col = 0; col < b->ss; col++) {
             if (b->board[row][col].poss_nb != 1) {
                 return false;
             }
+        }
+    }
 
     for (uint32_t i = 0; i < b->clue_nb; i++)
         if (!clue_checker(b, i))
